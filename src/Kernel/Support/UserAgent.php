2905bddb6bff428b22ad6a01103c312a2c5585b6
Endy <281591@qq.com>
2023-08-18 15:03:35 +0800

    Update UserAgent.php

--- src/Kernel/Support/UserAgent.php
+++ src/Kernel/Support/UserAgent.php
@@ -43,7 +43,7 @@ class UserAgent
         }
 
         if (class_exists(InstalledVersions::class)) {
-            array_unshift($value, 'easywechat-sdk/'.((string) InstalledVersions::getVersion('w7corp/easywechat')));
+            array_unshift($value, 'easywechat-sdk/'.((string) InstalledVersions::getVersion('evit/easywechat')));
         }
 
         return trim(implode(' ', $value));
