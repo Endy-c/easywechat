e4a52b814fb02b35095822be55a7e1244913eb44
Endy <281591@qq.com>
2024-04-14 12:34:48 +0800

    Fix: 推送信息配置成JSON格式时的兼容

--- src/Kernel/Traits/DecryptXmlMessage.php
+++ src/Kernel/Traits/DecryptXmlMessage.php
@@ -6,7 +6,11 @@ use EasyWeChat\Kernel\Encryptor;
 use EasyWeChat\Kernel\Exceptions\BadRequestException;
 use EasyWeChat\Kernel\Message;
 use EasyWeChat\Kernel\Support\Xml;
+use JsonException;
 
+/**
+ * @SuppressWarnings("StaticAccess")
+ */
 trait DecryptXmlMessage
 {
     /**
@@ -23,17 +27,38 @@ trait DecryptXmlMessage
         $ciphertext = $message->Encrypt;
 
         $this->validateSignature($encryptor->getToken(), $ciphertext, $signature, $timestamp, $nonce);
-
-        $message->merge(Xml::parse(
-            $encryptor->decrypt(
-                ciphertext: $ciphertext,
-                msgSignature: $signature,
-                nonce: $nonce,
-                timestamp: $timestamp
-            )
-        ) ?? []);
+        $decrypted = $encryptor->decrypt(
+            ciphertext: $ciphertext,
+            msgSignature: $signature,
+            nonce: $nonce,
+            timestamp: $timestamp
+        );
+        if ($this->isJson($decrypted)) {
+            $message->merge(json_decode($decrypted, true));
+            return $message;
+        }
+        $message->merge(Xml::parse($decrypted) ?? []);
 
         return $message;
+    }
+
+    protected function isJson($value)
+    {
+        if (!is_string($value)) {
+            return false;
+        }
+
+        if (function_exists('json_validate')) {
+            return json_validate($value, 512);
+        }
+
+        try {
+            json_decode($value, true, 512, JSON_THROW_ON_ERROR);
+        } catch (JsonException) {
+            return false;
+        }
+
+        return true;
     }
 
     /**
